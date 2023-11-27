40cc6206a081168be1abc42606ff67e4754e433d
Paul Holsters <paul.holsters@hotmail.com>
2023-11-27 16:43:03 +0100

    toevoegen van crudacties

--- app-configuration/crud-actions.ts
+++ app-configuration/crud-actions.ts
@@ -3,14 +3,34 @@ import * as edgedb from "edgedb"
 import e from "./../dbschema/edgeql-js"
 export const crudActions:CrudAction[] = []
 const client = edgedb.createClient()
-const getAllProducts = async function getAllProducts(){
-    await client.ensureConnected()
-    return e.select(e.Movie, ()=>({
-        id: true,
-        title: true,
-        actors: { name: true },
-        release_year:true
-    })).run(client)
-}
-const getAllProductsCrudAction = new CrudAction('getAllProducts',getAllProducts)
-crudActions.push(getAllProductsCrudAction)
+client.ensureConnected().then(()=> {
+    const getAllMovies = async function getAllMovies() {
+        return e.select(e.Movie, () => ({
+            id: true,
+            title: true,
+            actors: {name: true,character_name:true},
+            release_year: true
+        })).run(client)
+    }
+    const getAllMoviesCrudAction = new CrudAction('getAllMovies', getAllMovies)
+    const getAllShows = async function getAllShows() {
+        return e.select(e.Show, () => ({
+            id: true,
+            title: true,
+            num_seasons:true
+        })).run(client)
+    }
+    const getAllShowsCrudAction = new CrudAction('getAllShows', getAllShows)
+    const getAllSeasons = async function getAllSeasons() {
+        return e.select(e.Season, () => ({
+            id: true,
+            number: true,
+            show: true,
+        })).run(client)
+    }
+    const getAllSeasonsCrudAction = new CrudAction('getAllSeasons', getAllSeasons)
+
+
+    crudActions.push(getAllMoviesCrudAction)
+}).catch(err=>console.log(err))
+
