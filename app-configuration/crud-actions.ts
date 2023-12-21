6006bd64c12ea1d377898ce2dde46e0c6e0d2902
Paul Holsters <paul.holsters@hotmail.com>
2023-12-21 15:07:33 +0100

    toevoegen response verwijderen van lijst

--- app-configuration/crud-actions.ts
+++ app-configuration/crud-actions.ts
@@ -4,7 +4,6 @@ import e from "./../dbschema/edgeql-js"
 export const compileCommandsUserConfig = function compileCommandsUserConfig(client:edgedb.Client):CrudAction[]{
     const crudActions:CrudAction[] = []
     const getAllMovies = async function getAllMovies() {
-        debugger
         // todo add try catch for internal logging => indien fout geprogrammeerd kan dit fout lopen = niet type safe!
         // todo maak dit type safe met behulp van interface generators
         const myAccount = e.select(e.Account,(account)=>({
@@ -57,17 +56,28 @@ export const compileCommandsUserConfig = function compileCommandsUserConfig(clie
     crudActions.push(new CrudAction('getAllContent', getAllContent))
     const removeMovieFromList = async function removeMovieFromList(id:string|undefined) {
         if(id){
+            const myAccount = e.select(e.Account,(account)=>({
+                id:true,
+                watchlist:{id:true},
+                filter: e.op(account.username,'=','Pol')
+            }));
             const movie = e.select(e.Movie,(m)=>({
                 filter_single: {id: id}
             }))
-            // todo geef de nieuwe lijst door?
-            // todo of misschien beter enkel het aangepaste record?
-            return e.update(e.Account,(acc)=>({
+            e.update(e.Account,(acc)=>({
                 filter_single: {username: 'Pol'},
                 set: {
                     watchlist: {"-=":movie}
                 }
 
+            })).run(client)
+            return e.select(e.Movie,()=>({
+                id: true,
+                title: true,
+                actors: {name: true},
+                release_year: true,
+                isInList:e.bool(false),
+                filter_single: {id: id}
             })).run(client)
         }
         return null
